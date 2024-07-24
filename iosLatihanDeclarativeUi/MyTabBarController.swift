a2dbb795c4e305ce4a8d024ae2214f99ec1a6332
Andi Surya <andi502.as@gmail.com>
2024-07-24 17:03:08 +0700

    (feat): add tabbar, add color tab, add other view controller for tab

---
+++ iosLatihanDeclarativeUi/MyTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MyTabBarController.swift
+//  iosLatihanDeclarativeUi
+//
+//  Created by Andi Surya on 7/24/24.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
