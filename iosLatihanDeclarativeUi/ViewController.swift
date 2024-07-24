95e74640972b2d95ca4b41b2f5ac31738423d98f
Andi Surya <andi502.as@gmail.com>
2024-07-24 14:59:56 +0700

    Initial Commit

---
+++ iosLatihanDeclarativeUi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosLatihanDeclarativeUi
+//
+//  Created by Andi Surya on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
